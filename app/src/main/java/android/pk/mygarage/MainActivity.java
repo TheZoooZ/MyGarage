c281bc25d50fb0d0413f22762a9a84e32ec046b3
Tomasz Falarz <tomasz.falarz@pl.abb.com>
2020-06-20 14:07:06 +0200

    ListViewAdded

--- app/src/main/java/android/pk/mygarage/MainActivity.java
+++ app/src/main/java/android/pk/mygarage/MainActivity.java
@@ -1,14 +1,46 @@
 package android.pk.mygarage;
 
+import android.os.Bundle;
+import android.pk.mygarage.Models.ListViewAdapter;
+import android.widget.ListView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    private final String[] AssetNames = {
+            "Wiertarka",
+            "Sruba",
+            "Nóż",
+            "Korkociąg",
+            "Maczeta",
+            "Gogle",
+            "Piłka do metalu",
+            "Wygłuszacze",
+            "Wasserwaga",
+            "Metr"
+    };
+    private final String[] AssetDescriptions = {
+            "Dobra do wiercenia",
+            "Srednica 22mm",
+            "Badzo ostry, lepiej uważać",
+            "Wczoraj otwierałem nim szampana",
+            "Kupiłem u kolegów",
+            "Fajnie się komponuje z maczetą",
+            "Narzędzie do zadań specjalnych!",
+            "Moim uszom nic nie szkodzi!",
+            "Trzyma swój poziom...",
+            "Mierzę nim siły na zamiary"
+    };
+
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.listView);
+
+        ListViewAdapter adapter = new ListViewAdapter(this, AssetNames, AssetDescriptions);
+        listView.setAdapter(adapter);
     }
 }
