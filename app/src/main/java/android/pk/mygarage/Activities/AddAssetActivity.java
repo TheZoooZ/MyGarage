84dc6ff9148f90c489cfee41154ccf1d5365d713
Tomasz Falarz <tomasz.falarz@pl.abb.com>
2020-06-22 14:47:57 +0200

    Added AddAssetActivity layout

---
+++ app/src/main/java/android/pk/mygarage/Activities/AddAssetActivity.java
@@ -0,0 +1,15 @@
+package android.pk.mygarage.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.pk.mygarage.R;
+
+public class AddAssetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_asset);
+    }
+}
